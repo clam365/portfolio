e02b0e8124d3c309776fbbf4a79a865dd6357df1
chrislam365 <cllam@wpi.edu>
2024-07-02 20:23:50 -0400

    Fix

--- app/components/UserInterfaceFun.tsx
+++ app/components/UserInterfaceFun.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
 
 export default function UserInterfaceFun() {
     return (
-        <div>
+        <div className={"flex flex-col"}>
             <h1 className="font-semibold text-[#D5B895] text-sm mt-3 tracking-widest mb-10">
                 USER INTERFACE FUN
             </h1>
+            <div className={"flex gap-2 mb-20"}>
+                <Link href={"/kanban-board"}>
+                    <div>
+                        <h1 className={"text-2xl font-semibold"}>Kanban Board</h1>
+                    </div>
+                </Link>
+
+            </div>
             <hr/>
         </div>
     );
