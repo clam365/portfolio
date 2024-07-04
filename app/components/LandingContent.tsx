085eecbc8bfb8030de173b0148ec0637cb081a7a
chrislam365 <cllam@wpi.edu>
2024-07-04 02:35:56 -0400

    Padding now looks great

--- app/components/LandingContent.tsx
+++ app/components/LandingContent.tsx
@@ -5,13 +5,13 @@ import ProjectSection2 from "@/app/components/ProjectSection2";
 export default function LandingContent() {
     return (
 
-        <div className={"p-10 lg:p-40 pt-0 xl:pt-10 lg:pt-10"}>
+        <div className={"p-10 lg:p-28 pt-0 xl:pt-10 lg:pt-10"}>
             {/* Introduction */}
             <div className={"text-transparent bg-clip-text bg-gradient-to-r from-gray-400 to-[#121212]"}>
                 <h1 className="font-semibold text-lg md:text-xl lg:text-2xl mb-5">
                     Hello there!
                 </h1>
-                <p className="font-semibold text-xl md:text-2xl lg:text-4xl">
+                <p className="font-semibold text-2xl md:text-3xl  xl:text-4xl">
                     Chris is a full stack software engineer with a focus in frontend and UI/UX work, crafting meaningful
                     user interfaces that feel natural and free-flowing. He currently
                     is an intern with the Air Force.
